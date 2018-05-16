243d63a4a0100c8eac201814189f4e97a2888925
michailx <michail.xirouchakis@gmail.com>
2018-05-16 10:57:14 +0200

    Added linear, quadratic and burst trace
    Signed-off-by: michailx <michail.xirouchakis@gmail.com>

--- create_custom_trace.py
+++ create_custom_trace.py
@@ -3,6 +3,7 @@
 
 from scapy.all import *
 from time import time
+from math import sqrt
 
 
 ICMP_DATA = "michail dot xirouchakis at gmail dot com " * 34
@@ -19,14 +20,14 @@ def create_icmp_pkt(src_ip, dst_ip, data=ICMP_DATA, timestamp=time()):
     return packet
 
 
-def create_pkts(src_ip, dst_ip, pps):
+def create_pkts(src_ip, dst_ip, max_pps, curve_type="constant"):
     """
-    Returns a list of packets (ICMP Echo Requests). Each packet is sent at the appropriate time so that the user-defined
-    rate (pps) is met.
+    Returns a list of packets (ICMP Echo Requests). User must define the curve type (e.g., constant, linear, quadratic,
+    bursts) and the maximum throughput (pps).
     """
 
     list_of_packets = []
-    list_of_timestamps = create_timestamps(pps)
+    list_of_timestamps = create_timestamps(max_pps, curve_type)
 
     print "Creating ICMP Echo Requests."
     for timestamp in list_of_timestamps:
@@ -36,24 +37,70 @@ def create_pkts(src_ip, dst_ip, pps):
     return list_of_packets
 
 
-def create_timestamps(pps, runtime=TEST_DURATION_SEC):
+def create_timestamps(max_pps, curve_type, runtime=TEST_DURATION_SEC):
     """
-    Returns a list of timestamps (float; seconds since epoch). Consecutive timestamps are appropriately spaced so that
-    the user-defined rate (pps) is met.
+    Returns a list of timestamps (float; seconds since epoch).  User must define the curve type (e.g., constant, linear,
+    quadratic, bursts) and the maximum throughput (pps).
     """
 
-    print "Creating timestamps for generated packets. User-defined pps is:", pps
+    # Check input variables
+    if max_pps == 0:
+        print "You set input parameter max_pps to", max_pps, ". This is invalid. Exiting..."
+        sys.exit()
+    if curve_type not in ["constant", "bursts", "linear", "quadratic"]:
+        print "You set input parameter curve_type to", curve_type, ". This is invalid. Exiting..."
+        sys.exit()
+
+    print "Creating timestamps for generated packets. curve_type:", curve_type, ", Maximum throughput (pps):", max_pps
     start_time = time()
     list_of_timestamps = []
 
-    try:
-        interpacket_gap = 1.0 / pps
-    except ZeroDivisionError:
-        print "You set input parameter pps to 0. This is invalid. Exiting..."
-        sys.exit()
+    if curve_type == "constant":
+        interpacket_gap = 1.0 / max_pps
 
-    for timestamp in range(0, runtime*pps):
-        list_of_timestamps.append(start_time + (interpacket_gap*timestamp))
+        for timestamp in range(0, runtime*max_pps):
+            list_of_timestamps.append(start_time + (interpacket_gap*timestamp))
+
+    elif curve_type == "bursts":
+        interpacket_gap = 1.0 / max_pps
+
+        # Don't send a single packet every other second.
+        send_flag = False
+        for timestamp in range(0, runtime*max_pps):
+
+            if timestamp % max_pps == 0:
+                # Flip the send_flag after each second.
+                send_flag = not send_flag
+
+            if send_flag:
+                list_of_timestamps.append(start_time + (interpacket_gap*timestamp))
+
+    elif curve_type == "linear":
+        packets = range(1, max_pps+1)  # List will first element 1 and last element equal to max_pps
+
+        for k, v in enumerate(packets):
+
+            if v == 0:
+                continue
+            else:
+                interpacket_gap = 1.0 / v
+
+            for timestamp in range(0, v):  # Will not execute for timestamp equal to v
+                list_of_timestamps.append(start_time + k + (interpacket_gap * timestamp))
+
+    elif curve_type == "quadratic":
+        # List will first element 1 and last element equal to max_pps
+        packets = [i ** 2 for i in range(1, int(sqrt(max_pps))+1)]
+
+        for k, v in enumerate(packets):
+
+            if v == 0:
+                continue
+            else:
+                interpacket_gap = 1.0 / v
+
+            for timestamp in range(0, v):  # Will not execute for timestamp equal to v
+                list_of_timestamps.append(start_time + k + (interpacket_gap * timestamp))
 
     return list_of_timestamps
 
@@ -68,4 +115,4 @@ def export_trace(packets_list, filename="trace_"+str(int(time()))+".pcap"):
 
 
 if __name__ == "__main__":
-    export_trace(create_pkts("10.0.0.1", "10.0.0.2", 100))
+    export_trace(create_pkts("10.0.0.1", "10.0.0.2", 100, "bursts"))
