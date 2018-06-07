083a3f5b34c16e0fce2a9710099aa5d3c029c2f1
michailx <michail.xirouchakis@gmail.com>
2018-06-07 18:28:32 +0200

    User-defined custom series
    User can now define custom series by adding or multiplying
    several components, e.g., a sine signal with a linear trend.
    
    Signed-off-by: michailx <michail.xirouchakis@gmail.com>

--- create_custom_trace.py
+++ create_custom_trace.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from curve_functions import *
+import numpy
 from scapy.all import *
 from time import time
 
 ICMP_DATA = "michail dot xirouchakis at gmail dot com " * 34
 TEST_DURATION_SEC = 300
-CURVE_TYPES = {'constant': constant, 'linear': linear, 'quadratic': quadratic, 'sine': sine,
-               'noise': deterministic_noise}
 
 
 def create_icmp_pkt(src_ip, dst_ip, data=ICMP_DATA, timestamp=time()):
@@ -21,14 +20,12 @@ def create_icmp_pkt(src_ip, dst_ip, data=ICMP_DATA, timestamp=time()):
     return packet
 
 
-def create_pkts(src_ip, dst_ip, max_pps, curve_type='constant'):
+def _create_pkts(src_ip, dst_ip, list_of_timestamps):
     """
     Returns a list of packets (ICMP Echo Requests).
     """
 
     list_of_packets = []
-
-    list_of_timestamps = _create_timestamps(CURVE_TYPES[curve_type], max_pps)
 
     print "Creating ICMP Echo Requests."
     for timestamp in list_of_timestamps:
@@ -38,16 +35,27 @@ def create_pkts(src_ip, dst_ip, max_pps, curve_type='constant'):
     return list_of_packets
 
 
-def _create_timestamps(curve_function, max_pps, start_time=time(), run_time=TEST_DURATION_SEC):
+def _timeseries_to_timestamps(list_of_pps, start_time=time()):
     """
-    Returns a list of timestamps (float; seconds since epoch).
-    """
-    # Check input variables
-    if max_pps <= 0:
-        print "You set input parameter max_pps to", max_pps, ". This is invalid. Exiting..."
-        sys.exit()
+    Returns a list of floats; each element in this list is a packet's timestamp since start_time.
 
-    return curve_function(max_pps, start_time, run_time)
+    The function has two input arguments. list_of_pps is a list of integers; each element in this list is number of
+    packets during that second. Optional start_time is the timestamp of the first packet, set to current time since
+    Epoch by default.
+    """
+    timestamps = []
+
+    for timeslot, pkts in enumerate(list_of_pps):
+        if pkts == 0:
+            continue
+
+        # Each timeslot (which equals 1 second) has a different interpacket_gap equal to:
+        interpacket_gap = 1.0 / pkts
+
+        for pkt_index in range(0, pkts):
+            timestamps.append(start_time + timeslot + (pkt_index * interpacket_gap))
+
+    return timestamps
 
 
 def export_trace(packets_list, filename="trace_"+str(int(time()))+".pcap"):
@@ -59,23 +67,55 @@ def export_trace(packets_list, filename="trace_"+str(int(time()))+".pcap"):
     wrpcap(filename, packets_list)
 
 
+def user_defined_series():
+    # FIXME: As far as defining the individual signals, I need to provide a better UI to the end-user
+
+    # Here I just start the series with a periodic signal (sine) and some noise, and include a linear trend after 60 sec
+
+    s1 = numpy.array(sine(20, TEST_DURATION_SEC))
+    s2 = numpy.array(deterministic_noise(5, TEST_DURATION_SEC))
+    # 60 seconds later
+    s3 = numpy.array([0]*60 + linear(20, TEST_DURATION_SEC-60))  # Add zeros as padding
+
+    additive_curve = s1 + s2 + s3
+
+    # FIXME: I will add 1 packet to all time slots in order to avoid zeroes in multiplicative mode.
+    ones = numpy.array([1]*TEST_DURATION_SEC)
+    s1 += ones
+    s2 += ones
+    s3 += ones
+    multiplicative_curve = s1 * s2 * s3
+
+    return additive_curve, multiplicative_curve
+
+
 if __name__ == "__main__":
     print "Running script", sys.argv[0]
     print "User provided", (len(sys.argv)-1), "command-line arguments:"
 
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 4:
         print str(sys.argv[1:])
         print "These arguments are invalid. Exiting..."
         sys.exit()
 
-    user_input = {'src_ip': sys.argv[1], 'dst_ip': sys.argv[2], 'max_pps': int(sys.argv[3]), 'curve_type': sys.argv[4]}
+    user_input = {'src_ip': sys.argv[1], 'dst_ip': sys.argv[2], 'max_pps': int(sys.argv[3])}
     print "* Source IP:", user_input['src_ip']
     print "* Destination IP:", user_input['dst_ip']
     print "* Maximum Throughput (pps):", user_input['max_pps']
-    print "* Curve:", user_input['curve_type']
 
-    if user_input['curve_type'] not in CURVE_TYPES.keys():
-        print "Only supporting curves", CURVE_TYPES.keys(), ". Exiting..."
-        sys.exit()
+    current_time = time()
 
-    export_trace(create_pkts(**user_input), "trace_"+str(user_input['curve_type'])+"_"+str(int(time()))+".pcap")
+    # User defined series; see function user_defined_series()
+    additive_curve, multiplicative_curve = user_defined_series()
+
+    # Composition Mode
+    # Additive
+    timestamps = _timeseries_to_timestamps(additive_curve, current_time)
+    export_trace(_create_pkts(user_input['src_ip'], user_input['dst_ip'], timestamps),
+                 "trace_additive_"+str(int(current_time))+".pcap")
+
+    # Multiplicative:
+    timestamps = _timeseries_to_timestamps(multiplicative_curve, current_time)
+    export_trace(_create_pkts(user_input['src_ip'], user_input['dst_ip'], timestamps),
+                 "trace_multiplicative_"+str(int(current_time))+".pcap")
+
--- curve_functions.py
+++ curve_functions.py
@@ -10,57 +10,25 @@ SEED_VALUE = 1337  # The value itself is not significant. What is significant is
 # all calls of function deterministic_noise()
 
 
-def constant(pps, start_time, run_time_sec):
-    timestamps = []
-    interpacket_gap = 1.0 / pps
-
-    for pkt_index in range(0, run_time_sec * pps):
-        timestamps.append(start_time + (interpacket_gap * pkt_index))
-
-    return timestamps
+def constant(max_pps, run_time_sec):
+    # All seconds of the test will have pps = max_pps, so:
+    pps = [max_pps for _ in range(1, run_time_sec + 1)]
+    return pps
 
 
-def linear(max_pps, start_time, run_time_sec):
-    timestamps = []
-
+def linear(max_pps, run_time_sec):
     # The last second of the test must have pps == max_pps, so:
     pps = [(i * max_pps) / run_time_sec for i in range(1, run_time_sec + 1)]
-
-    for timeslot, pkts in enumerate(pps):
-        if pkts == 0:
-            continue
-
-        # Each timeslot (which equals 1 second) has a different interpacket_gap equal to:
-        interpacket_gap = 1.0 / pkts
-
-        for pkt_index in range(0, pkts):
-            timestamps.append(start_time + timeslot + (pkt_index * interpacket_gap))
-
-    return timestamps
+    return pps
 
 
-def quadratic(max_pps, start_time, run_time_sec):
-    timestamps = []
-
+def quadratic(max_pps, run_time_sec):
     # The last second of the test must have pps == max_pps, so:
     pps = [int(((i / float(run_time_sec)) ** 2) * max_pps) for i in range(1, run_time_sec + 1)]
-
-    for timeslot, pkts in enumerate(pps):
-        if pkts == 0:
-            continue
-
-        # Each timeslot (which equals 1 second) has a different interpacket_gap equal to:
-        interpacket_gap = 1.0 / pkts
-
-        for pkt_index in range(0, pkts):
-            timestamps.append(start_time + timeslot + (pkt_index * interpacket_gap))
-
-    return timestamps
+    return pps
 
 
-def sine(max_pps, start_time, run_time_sec):
-    timestamps = []
-
+def sine(max_pps, run_time_sec):
     # So the pps values will flactuate between max_pps and 0:
     pps_period = [int(max_pps * (sin(radians(i - 90)) + 1)/2) for i in
                   [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 270, 292.5, 315, 337.5]]
@@ -69,39 +37,15 @@ def sine(max_pps, start_time, run_time_sec):
     periods = run_time_sec / len(pps_period)
     modulo = run_time_sec % len(pps_period)
     pps = pps_period * periods + pps_period[:modulo]
-
-    for timeslot, pkts in enumerate(pps):
-        if pkts == 0:
-            continue
-
-        # Each timeslot (which equals 1 second) has a different interpacket_gap equal to:
-        interpacket_gap = 1.0 / pkts
-
-        for pkt_index in range(0, pkts):
-            timestamps.append(start_time + timeslot + (pkt_index * interpacket_gap))
-
-    return timestamps
+    return pps
 
 
-def deterministic_noise(max_pps, start_time, run_time_sec):
-    timestamps = []
-
+def deterministic_noise(max_pps, run_time_sec):
     seed(SEED_VALUE)  # Running deterministic_noise() w/out changing the SEED_VALUE will always produce the same result
 
     # The last second of the test must have pps == max_pps, so:
     pps = [randint(0, max_pps) for _ in range(1, run_time_sec + 1)]
-
-    for timeslot, pkts in enumerate(pps):
-        if pkts == 0:
-            continue
-
-        # Each timeslot (which equals 1 second) has a different interpacket_gap equal to:
-        interpacket_gap = 1.0 / pkts
-
-        for pkt_index in range(0, pkts):
-            timestamps.append(start_time + timeslot + (pkt_index * interpacket_gap))
-
-    return timestamps
+    return pps
 
 
 if __name__ == "__main__":
